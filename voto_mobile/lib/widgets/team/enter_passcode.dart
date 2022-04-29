7ca079cfc899d5175cf2795340daa3302b5b5c21
Aerith <chutirat.earth76@mail.kmutt.ac.th>
2022-04-29 18:23:05 +0700

    [ADD] Icon and Text

--- voto_mobile/lib/widgets/team/enter_passcode.dart
+++ voto_mobile/lib/widgets/team/enter_passcode.dart
@@ -1,2 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:voto_mobile/utils/color.dart';
+import 'dart:ui';
+
+class EnterPasscode extends StatelessWidget {
+  const EnterPasscode({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(
+          top: 20,
+          right: 25,
+          left: 15,
+        ),
+        child: Row(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.clear, color: VotoColors.primary),
+              onPressed: () {},
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+              ),
+              child: Text(
+                "Enter passcode",
+                style: GoogleFonts.inter(
+                    fontSize: 16,
+                    color: VotoColors.primary,
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+          ],
+        ));
+  }
+}
